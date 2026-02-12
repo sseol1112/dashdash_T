363f55199cb867228cb432772182423146970a96
sseol1112 <sseol1112@naver.com>
2026-02-12 14:06:13 +0900

    Updates

--- _next/static/qndeltKyCdzHSpo_yI4Bj/_buildManifest.js
+++ _next/static/qndeltKyCdzHSpo_yI4Bj/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dashdash_T//_next/:path+",
+        "destination": "/dashdash_T/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
